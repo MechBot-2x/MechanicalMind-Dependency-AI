d5e60f06dc7b4416fa5132a08161866dcafe5ec4
mechmind-dwv <ia.mechmind@gmail.com>
2025-04-09 22:51:37 +0200

    Actualizar setup_runner.sh

--- scripts/setup_runner.sh
+++ scripts/setup_runner.sh
@@ -14,7 +14,7 @@ curl -o actions-runner-linux-x64-2.323.0.tar.gz -L \
   https://github.com/actions/runner/releases/download/v2.323.0/actions-runner-linux-x64-2.323.0.tar.gz
 
 # 3. Verificar integridad
-echo "0dbc9bf5a58620fc52cb6cc0448abcca964a8d74b5f39773b7afcad9ab691e19  actions-runner-linux-x64-2.323.0.tar.gz" | shasum -a 256 -c
+echo "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 actions-runner-linux-x64-2.323.0.tar.gz" | shasum -a 256 -c
 
 # 4. Extraer
 tar xzf ./actions-runner-linux-x64-2.323.0.tar.gz
