363ad1940416c9514e37266d82bfea3099400492
mechmind-dwv <ia.mechmind@gmail.com>
2025-04-12 00:50:55 +0200

    Create __init__.py

---
+++ ai_core/utils/__init__.py
@@ -0,0 +1,36 @@
+"""
+MechanicalMind Utilities Package v2.0
+Utility functions for the Dependency AI system
+"""
+
+from .file_helpers import (
+    safe_read_file,
+    read_yaml,
+    write_json,
+    file_checksum,
+    find_files_by_pattern
+)
+from .network_helpers import (
+    check_internet_connection,
+    download_file,
+    get_pypi_package_info,
+    is_port_available
+)
+from .logging_config import (
+    configure_logging,
+    get_module_logger
+)
+
+__all__ = [
+    'safe_read_file',
+    'read_yaml',
+    'write_json',
+    'file_checksum',
+    'find_files_by_pattern',
+    'check_internet_connection',
+    'download_file',
+    'get_pypi_package_info',
+    'is_port_available',
+    'configure_logging',
+    'get_module_logger'
+]
