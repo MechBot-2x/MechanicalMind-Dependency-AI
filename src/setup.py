cf9d9bccfdef0432cf6aa3d4708a1c4e012ba6b7
mechmind-dwv <ia.mechmind@gmail.com>
2025-04-18 16:51:23 +0200

    Crear src/setup.py

---
+++ src/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MechanicalMindDependencyAI',
+    version='1.0.0',
+    packages=find_packages(),
+    install_requires=[
+        # Add dependencies here
+    ],
+    description='Description of your project',
+    author='Your Name',
+    author_email='your-email@example.com',
+    url='https://github.com/mechmind-dwv/-MechanicalMind-Dependency-AI-v3.0-',
+)
