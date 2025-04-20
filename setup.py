46bf3cbb851ec9c87434332de9e5c0bfc2aa495d
mechmind-dwv <ia.mechmind@gmail.com>
2025-04-20 11:01:57 +0200

    Actualizar setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
         # Add dependencies here
     ],
     description='Description of your project',
-    author='mechmind-dwv,
+    author='mechmind-dwv',
      author_email='ai.mechmind@gmail.com',
     url='https://github.com/mechmind-dwv/-MechanicalMind-Dependency-AI-v3.0-',
 )
